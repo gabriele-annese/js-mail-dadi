9b4b9f093f6615dbafd72c58ad724069db499a53
gabriele-annese <gannese06@gmail.com>
2021-10-26 13:42:41 +0200

    add verifica email

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@
 const email = ['cicciobello02@gmail.com','pasquale87@gmail.com','claudia23@hotmail.com',];
 const emailInput = document.querySelector("#email");
 const accessBtn = document.querySelector(".btn");
+const accesso = document.querySelector(".accesso")
 // - chiedi all’utente la sua email
 
 accessBtn.addEventListener('click', function() {
     // otteniamo l email dell'utente
     const userEmail = emailInput.value.trim().toLowerCase()
     console.log(userEmail)
+    // verifacere useremail in array
+    let emailFoun = false;
+    for(let i = 0; i < email.length; i++){
+        if(email[i] == userEmail){
+            emailFoun = true;
+            break;
+        }
+    }
+    console.log(emailFoun);
 }
 );
